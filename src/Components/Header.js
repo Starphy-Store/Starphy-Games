3b97df84c6bb5458bf83e33c4bccbfeada5d3c6a
L410N3L <57776908+L410N3L@users.noreply.github.com>
2021-11-16 13:15:49 -0500

    arreglado el bootstrap

---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+//Documentacion!
+//https://react-bootstrap.netlify.app/components/navbar/#navbars
+
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+
+function Header() {
+    return(
+        <Navbar bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+    )
+} 
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+  integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+  crossorigin="anonymous"
+/>
 
 ReactDOM.render(
   <React.StrictMode>
