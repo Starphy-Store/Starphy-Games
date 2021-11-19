a51baa5e5b377fbad8123de3cd1bf86408ea1d03
proyect_icaro <josuevalenzuela87@gmail.com>
2021-11-19 11:04:40 -0300

    Agregando animación

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@
 import Header from  "./Components/Header"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Slide from "./Components/Slide";
 
 function App() {
   return (
     <div className="bg">
     <Header></Header>
+    <Slide></Slide>
     </div>
   );
 }
---
+++ src/Components/Slide.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Slide = () => {
+    return (
+        
+           <div  className="animate__bounce" id="slide">
+             <h1>Descubre los mejores juegos
+             seleccionados para ti</h1>
+           </div>
+        
+    )
+}
+
+export default Slide
