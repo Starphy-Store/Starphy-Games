c83cfb5ca74c647be74266a6194377302fee3706
guillermoBabangit <guillermo.baban@hotmail.com>
2021-12-01 18:49:39 -0300

    react-router && login page finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 //DOCUMENTACION!
 //https://react-bootstrap.github.io/getting-started/introduction
 
-import Header from  "./Components/Header"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import Header from "./Components/Header";
+import Login from "./Components/login/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+// import Login from "./Components/login/Login";
+// import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="bg">
-    <Header></Header>
-    </div>
+    <Routes>
+      <Route path="/navbar" element={<Header />} />
+      <Route path="/" element={<Login />} />
+    </Routes>
   );
 }
 
+// <Container className="loginContainer">
+//   <Login />
+// </Container>
 export default App;
---
+++ src/Components/login/assets/index.js
@@ -0,0 +1,5 @@
+import eyeIcon from "../assets/eye.png";
+import google from "../assets/google.png";
+import facebook from "../assets/facebook.png";
+
+export { eyeIcon, google, facebook };
