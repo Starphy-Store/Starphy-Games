0f7b75545defead49f1e433b69e06f6937a481cb
ian <ianjosejf@gmail.com>
2021-12-19 06:12:52 +0100

    jugando a jugar play

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 //importacion del header
 import Header from "../Components/Nav/Header";
 
 //imagenes
-import Barrita from "https://i.postimg.cc/9f5R5HX9/Dise-o-sin-t-tulo-6-removebg-preview.png";
+/* import Barrita from "https://i.postimg.cc/9f5R5HX9/Dise-o-sin-t-tulo-6-removebg-preview.png"; */
 
 //importacion de las Cards
 import Slider from "./Slider";
@@ -11,21 +12,18 @@ import Slider from "./Slider";
 //Slider de recomendaciones
 import Recomendations1 from "./Recomendations1";
 
-const home = () => {
+const Home = ({ game }) => {
   return (
     <div>
       <Slider />
       <Header />
       <div style={{ marginTop: "70vh" }}>
-        <img src={Barrita}></img>
+        {/*  <img src={Barrita}></img> */}
         <h1 style={{ color: "white" }}>Los mejores juegos para ti</h1>
       </div>
       <Recomendations1 numCol="3" />
       <h1>Juegos multijugador 👋</h1>
-      <Recomendations1 numCol="4" />
-      <Recomendations1 numCol="3" />
-      <Recomendations1 numCol="3" />
     </div>
   );
 };
-export default home;
+export default Home;
