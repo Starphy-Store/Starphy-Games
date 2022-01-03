ee072620bd4713b9e91f0ab1aedc3112f2afc629
ian <ianjosejf@gmail.com>
2022-01-03 21:21:45 +0100

    arreglado

--- src/Library/Library.js
+++ src/Library/Library.js
@@ -1,2 +1,12 @@
 import React from "react";
 import Header from "../Components/Nav/Header";
+
+function Library() {
+  return (
+    <>
+      <Header />
+      <h1 style={{ textAlign: "left", paddingLeft: "40px" }}> Tu libreria</h1>
+    </>
+  );
+}
+export default Library;
