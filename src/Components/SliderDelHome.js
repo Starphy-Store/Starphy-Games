17a1ba7d6de785543a1fc9c4cd2cc9c56a99d09f
ian <ianjosejf@gmail.com>
2022-03-20 10:34:11 +0100

    poco a poco

--- src/Components/SliderDelHome.js
+++ src/Components/SliderDelHome.js
@@ -16,7 +16,7 @@ function SliderDelHome() {
       <Carousel
         variant="light"
         indicators={false}
-        style={{ zIndex: "0", borderRadius: "10px" }}
+        style={{ borderRadius: "10px" }}
         className="carousel-inner"
       >
         <Carousel.Item>
@@ -27,10 +27,10 @@ function SliderDelHome() {
             </Container>
           </div> */}
           <img
-            className="d-block"
+            className="d-block sliderImg"
             src={payday2}
-            className="sliderImg"
             alt="First slide"
+            style={{ height: "auto", width: "100%" }}
           />
         </Carousel.Item>
         <Carousel.Item>
@@ -41,10 +41,10 @@ function SliderDelHome() {
             </Container>
           </div> */}
           <img
-            className="d-block"
+            className="d-block sliderImg"
             src="https://firebasestorage.googleapis.com/v0/b/usuarios-b78e1.appspot.com/o/Publica%20tus%20juegos%20gratis%20(1).png?alt=media&token=17af5675-45b7-4e7b-858e-70f5fbb6b3d1"
             alt="Second slide"
-            className="sliderImg"
+            style={{ height: "auto", width: "100%" }}
           />
         </Carousel.Item>
         <Carousel.Item>
@@ -55,10 +55,10 @@ function SliderDelHome() {
             </Container>
           </div> */}
           <img
-            className="d-block"
+            className="d-block sliderImg"
             src={thewitcher}
             alt="Third slide"
-            className="sliderImg"
+            style={{ height: "auto", width: "100%" }}
           />
         </Carousel.Item>
       </Carousel>
